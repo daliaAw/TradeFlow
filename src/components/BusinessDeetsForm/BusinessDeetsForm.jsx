a6fceecf3832a16544aec7ad354747fa117c8ac6
Felix <andrewchris99@yahoo.com>
2024-04-10 09:07:55 -0400

    first commit

---
+++ src/components/BusinessDeetsForm/BusinessDeetsForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function BusinessDeetsForm() {
+  return <div>BusinessDeetsForm</div>;
+
+//   CREATE FORM FOR BUSINESS DETAILS THEN RENDER TO BUSINESS ACCOUNT PAGE
+}
+
+export default BusinessDeetsForm;
--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,36 +1,37 @@
-import { Component } from 'react';
-import { signUp } from '../../utilities/users-service';
+import { Component } from "react";
+import { signUp } from "../../utilities/users-service";
 
 export default class SignUpForm extends Component {
   state = {
-    name: '',
-    email: '',
-    password: '',
-    confirm: '',
-    error: ''
+    name: "",
+    email: "",
+    password: "",
+    confirm: "",
+    error: "",
   };
 
   handleChange = (evt) => {
     this.setState({
       [evt.target.name]: evt.target.value,
-      error: ''
+      error: "",
     });
   };
 
   handleSubmit = async (evt) => {
     evt.preventDefault();
     try {
-      const {name, email, password} = this.state;
-      const formData = {name, email, password};
+      const { name, email, password } = this.state;
+      const formData = { name, email, password };
       // The promise returned by the signUp service
       // method will resolve to the user object included
       // in the payload of the JSON Web Token (JWT)
       const user = await signUp(formData);
       this.props.setUser(user);
+      // IFF USER IS FALSE GO TO MAIN PAGE ELSE GO TO BUSINESS DETAILS PAGE
     } catch {
       // An error occurred
       // Probably due to a duplicate email
-      this.setState({ error: 'Sign Up Failed - Try Again' });
+      this.setState({ error: "Sign Up Failed - Try Again" });
     }
   };
 
@@ -41,14 +42,41 @@ export default class SignUpForm extends Component {
         <div className="form-container">
           <form autoComplete="off" onSubmit={this.handleSubmit}>
             <label>Name</label>
-            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              required
+            />
             <label>Email</label>
-            <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              required
+            />
             <label>Password</label>
-            <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              required
+            />
             <label>Confirm</label>
-            <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-            <button type="submit" disabled={disable}>SIGN UP</button>
+            <input
+              type="password"
+              name="confirm"
+              value={this.state.confirm}
+              onChange={this.handleChange}
+              required
+            />
+            {/* ADD BOOLEAN INPUT FOR BUSINESS */}
+            <button type="submit" disabled={disable}>
+              SIGN UP
+            </button>
           </form>
         </div>
         <p className="error-message">&nbsp;{this.state.error}</p>
