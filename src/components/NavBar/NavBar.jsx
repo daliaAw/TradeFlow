5f4426ff78ce671791b76ec16cfbcd524282e030
Felix <andrewchris99@yahoo.com>
2024-04-10 11:17:36 -0400

    update links in navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service';
+import { Link } from "react-router-dom";
+import * as userService from "../../utilities/users-service";
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -8,13 +8,35 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      &nbsp;&nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
-    </nav>
+    <>
+      <div>
+        <h2>Trade Flow</h2>
+        &nbsp; &nbsp;
+        <h3>Search bar will go here</h3>
+        &nbsp; &nbsp;
+        {user ? (
+          <Link to="" onClick={handleLogOut}>
+            Log Out
+          </Link>
+        ) : (
+          <Link>Login</Link> / <Link>Sign Up</Link>
+        )}
+      </div>
+      <nav>
+        <Link to="/categories/consumergoods">Consumer Goods</Link>
+        &nbsp; | &nbsp;
+        <Link to="/categories/technologyelectronics">
+          Technology and Electronics
+        </Link>
+        &nbsp; | &nbsp;
+        <Link to="/categories/fashionapparel">Fashion and Apparel</Link>
+        &nbsp; | &nbsp;
+        <Link to="/categories/homegarden">Home and Garden</Link>
+        &nbsp; | &nbsp;
+        <Link to="/categories/healthwellness">Health and Wellness</Link>
+        &nbsp; | &nbsp; &nbsp;&nbsp;
+        <span>Welcome, {user.name}</span>
+      </nav>
+    </>
   );
 }
--- src/pages/SingleCategory/SingleCategory.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function SingleCategory() {
-  return <div>SingleCategory</div>;
-}
-
-export default SingleCategory;
