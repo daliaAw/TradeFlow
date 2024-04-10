bd7ae53305e50967215ec2b51d2bab0eda15cbe7
charanskumar <charansenthilk@gmail.com>
2024-04-10 16:40:09 -0400

    Show categories list on categories page

--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -1,5 +1,36 @@
+import React from "react";
+import CategoryCard from "../../components/CategoryCard/CategoryCard";
+// import CategoryCard from '../components/CategoryCard';
+
+const categories = [
+    {
+        name: 'Consumer Goods'
+    },
+    {
+        name: 'Technology and Electronics'
+    },
+    {
+        name: 'Fashion and Apparel'
+    },
+    {
+        name: 'Home and Garden'
+    },
+    {
+        name: 'Health and Wellness'
+    }
+]
+
 export default function CategoriesPage() {
     return (
-        <h1>Categories</h1>
+        <>
+            <h1>All Categories</h1>
+            <div className="categoriesPage">
+                {categories.map(c => (
+                    <CategoryCard
+                        name={c.name}
+                    />
+                ))}
+            </div>
+        </>
     );
 }
