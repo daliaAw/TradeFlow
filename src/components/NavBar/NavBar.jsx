f07525ab39664ef7a420eb7481666e971b807814
treyweldon <trey@millcreeklimited.com>
2024-04-11 09:42:59 -0400

    nav fix

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,10 +12,7 @@ export default function NavBar({ user, setUser }) {
 
   const [categories, setCategories] = useState([
     { name: "Consumer Goods", path: "categories/consumergoods" },
-    {
-      name: "Technology and Electronics",
-      path: "categories/technologyelectronics",
-    },
+    { name: "Technology and Electronics", path: "categories/technologyelectronics",},
     { name: "Fashion and Apparel", path: "categories/fashionapparel" },
     { name: "Home and Garden", path: "categories/homegarden" },
     { name: "Health and Wellness", path: "categories/healthwellness" },
@@ -32,15 +29,13 @@ export default function NavBar({ user, setUser }) {
         <h3>Search bar will go here</h3>
         &nbsp; &nbsp;
         <>
-          <nav>
-            <ul className="navUL">
-              {categories.map((category) => (
-                <li key={category.path}>
-                  <Link to={category.path}>{category.name}</Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
+        <nav>
+            {categories.map((category) => (
+              <span key={category.name}>
+                <Link to={`categories/${category.name}`} >{category.name}</Link> &nbsp; &nbsp;
+              </span>
+            ))}
+        </nav>
         </>
         {user ? (
           <>
@@ -48,31 +43,23 @@ export default function NavBar({ user, setUser }) {
             <Link to="" onClick={handleLogOut}>
               Log Out
             </Link>
-            &nbsp; &nbsp;&nbsp; &nbsp;
+            &nbsp; &nbsp;&nbsp;
             <span>
               Welcome,
               <Link to="/profile">{user.name}</Link>
-              <Link to="/create">New Product</Link>
+              &nbsp; &nbsp;<Link to="/create">New Product</Link>
             </span>
 
           </>
 
         ) : (
           <>
-            <Link to="/">Login/Sign Up</Link>
+            <Link to="">Login/Sign Up</Link>
           </>
         )}
       </div>
       <>
-        <nav>
-          
-            {categories.map((category) => (
-              <span key={category.name}>
-                <Link to={`categories/${category.name}`} setCategories={setCategories} >{category.name}</Link> &nbsp; &nbsp;
-              </span>
-            ))}
-          
-        </nav>
+
       </>
        &nbsp; &nbsp;&nbsp;
     </>
