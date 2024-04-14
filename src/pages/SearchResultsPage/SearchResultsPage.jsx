38d6848ddd3a4769f0035cc76ac01ece0a45868c
treyweldon <trey@millcreeklimited.com>
2024-04-14 11:00:34 -0400

    no results condition

--- src/pages/SearchResultsPage/SearchResultsPage.jsx
+++ src/pages/SearchResultsPage/SearchResultsPage.jsx
@@ -8,19 +8,21 @@ export default function SearchResultsPage({ products }){
     product.description.toLowerCase().includes(searchTerm.toLowerCase()))
 
     return(
-        <>
-        <h1>Search Results</h1>
-
-        {resultsArr.map((product) => (
-        <Link to={`/item/${product.category}/${product._id}`}>
-        <div className="item-card">
-            <p>{product.title}</p>
-            <p>${product.wholesalePrice}</p>
-        </div>
-        </Link>
-            ))}
-
-        </>
+    <>
+    <h1>Search Results</h1>
+    {resultsArr.length === 0 ? (
+        <p>No Results Found</p>
+    ) : (
+        resultsArr.map((product) => (
+            <Link to={`/item/${product.category}/${product._id}`} key={product._id}>
+                <div className="item-card">
+                    <p>{product.title}</p>
+                    <p>${product.wholesalePrice}</p>
+                </div>
+            </Link>
+        ))
+    )}
+    </>
     )
 }
 
