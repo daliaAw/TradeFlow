47897fc79d11c8df34f94c6a4f6436284f99a2ae
charanskumar <charansenthilk@gmail.com>
2024-04-16 19:10:20 -0400

    Add some styling

--- src/components/FavoritesList/FavoritesList.jsx
+++ src/components/FavoritesList/FavoritesList.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ItemCard from "../ItemCard/ItemCard";
+import "./FavoritesList.css"
 
 export default function FavoritesList({ favorites }) {
   return (
-    <div>
+    <div className="favorites">
       <h2>Favorites:</h2>
       <ul>
+        <div className="favItems">
         {favorites.map((favorite) => (
           <li key={favorite._id}><ItemCard key={favorite._id} item={favorite} /></li>
         ))}
+        </div>
       </ul>
     </div>
   );
